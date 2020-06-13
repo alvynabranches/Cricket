66020cb53a206cb7d01eea59559d276fc5f026fd
Alvyn Abranches <alvynabranches@gmail.com>
2020-06-13 13:29:28 +0530

    13062020v1

--- cricket.py
+++ cricket.py
@@ -1,4 +1,4 @@
-class Team():
+class Team:
     def __init__(self, team_name):
         self.team_name = team_name
 
@@ -6,3 +6,17 @@ class Team():
 class Batsmen:
     def __init__(self, player_name):
         self.player_name = player_name
+
+class Bowler:
+    def __init__(self, player_name):
+        self.player_name = player_name
+
+class Score:
+    def __init__(self, match_type:str, batsmen1, batsmen2, runs=0, wickets=0, balls=0):
+        self.match_type = match_type
+        self.runs = runs
+        self.wickets = wickets
+        self.balls = balls
+        self.batsmen1 = bestmen1
+        self.batsmen2 = bestmen2
+        self.current_bestmen = [True, False]
