d66112b2f5a4fe3645368bbeb2ecfe8b9b04ade5
Alvyn Abranches <alvynabranches@gmail.com>
2020-06-13 14:46:53 +0530

    13062020v11

--- score.py
+++ score.py
@@ -1,14 +1,16 @@
 class Score_Batting_First:
-    def __init__(self, match_type:tuple, batsmen1:str, batsmen2:str, runs=0, wickets=0, balls=0):
+    def __init__(self, match_type:tuple, batsmen1:str, batsmen2:str, runs=0, wickets=0, balls=0, extras=0):
         '''
             match_type => takes in 2 parameter in a tuple i.e. (odi or test, no_of_overs or no_of_days)
         '''
         self.match_type = match_type
+        self.batsmen = [bestmen1, bestmen2]
+        self.current_batsmen = [True, False]
+
         self.runs = runs
         self.wickets = wickets
         self.balls = balls
-        self.batsmen = [bestmen1, bestmen2]
-        self.current_batsmen = [True, False]
+        self.extras = extras
     
     def update_score(self, runs, balls=1, wickets=0):
         self.runs += runs
@@ -48,7 +50,8 @@ class Score_Batting_First:
         current_batsmen[1] != current_batsmen[1]
 
     def noball(self, runs):
-        self.update_score(runs, 0)
+        self.update_score(runs+1, 0)
+        self.extras += (runs+1)
         if runs % 2 == 1:
             current_batsmen[0] != current_batsmen[0]
             current_batsmen[1] != current_batsmen[1]
@@ -67,16 +70,25 @@ class Score_Batting_First:
 
     def noball_run_out(self, completed_runs):
         self.update_score(completed_runs, 0, 1)
+        self.extras += (completed_runs+1)
         if completed_runs % 2 == 0:
             current_batsmen[0] != current_batsmen[0]
             current_batsmen[1] != current_batsmen[1]
 
     def stumped(self):
         self.update_score(0, 1, 1)
+    
+    def stumped_wide(self):
+        self.extras += 1
+        self.update_score(1, 0, 1)
+
+    def wide(self, runs):
+        self.extras += (runs+1)
+        self.update_score(runs, 0, 0)
 
 def Score_Batting_Second(Score_Batting_First):
-    def __init__(self, match_type:str, batsmen1, batsmen2, target, runs=0, wickets=0, balls=0):
-        super().__init__(match_type, batsmen1, batsmen2)
+    def __init__(self, match_type:str, batsmen1, batsmen2, target, runs=0, wickets=0, balls=0, extras=0):
+        super().__init__(match_type, batsmen1, batsmen2, runs, wickets, balls, extras)
         self.target = target
 
     def update_score(self, runs, balls=1, wickets=0):
@@ -120,3 +132,9 @@ def Score_Batting_Second(Score_Batting_First):
 
     def stumped(self):
         super().stumped()
+
+    def stumped_wide(self):
+        super().stumped_wide()
+
+    def wide(self):
+        super().wide()
