69045e2c9369a271759fb0eb18ff1650d09b075e
Alvyn Abranches <alvynabranches@gmail.com>
2020-06-13 14:11:49 +0530

    13062020v7

--- score.py
+++ score.py
@@ -47,3 +47,47 @@ class Score:
 
     def noball_run_out(self, completed_runs):
         self.update_score(completed_runs, 0, 1)
+
+def Score_Batting_Second(Score):
+    def __init__(self, match_type:str, batsmen1, batsmen2, target, runs=0, wickets=0, balls=0):
+        super().__init__(match_type, batsmen1, batsmen2)
+        self.target = target
+
+    def update_score(self, runs, balls=1, wickets=0):
+        super.update_score(runs, balls, wickets)
+
+    def zero(self):
+        super().zero()
+
+    def one(self):
+        super().one()
+    
+    def two(self):
+        super().two()
+
+    def three(self):
+        super().three()
+
+    def four(self):
+        super().four()
+
+    def five(self):
+        ssuper().five()
+
+    def six(self):
+        super().six()
+    
+    def seven(self):
+        super().seven()
+
+    def noball(self, runs):
+        super().noball(runs)
+
+    def catch_out(self, crossed:bool):
+        super().catch_out(crossed)
+    
+    def run_out(self, completed_runs):
+        super().run_out(completed_runs)
+
+    def noball_run_out(self, completed_runs):
+        super().noball_run_out(completed_runs)
