7a954d2a7ed3da54abf917295f74458254ab6ea6
Alvyn Abranches <alvynabranches@gmail.com>
2020-06-13 13:23:01 +0530

    13062020v1

---
+++ cricket.py
@@ -0,0 +1,8 @@
+class Team():
+    def __init__(self, team_name):
+        self.team_name = team_name
+
+
+class Batsmen:
+    def __init__(self, player_name):
+        self.player_name = player_name
